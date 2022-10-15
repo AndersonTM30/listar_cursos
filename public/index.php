df860732720cd783f996c7c3f28bd29c1d80248f
AndersonTM30 <andersondev30@gmail.com>
2022-10-15 18:10:46 -0300

    Criando o index.php e fazendo a configuração das páginas de listar cursos, novo curos e 404

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+    // redirecionamento de páginas
+    switch($_SERVER['PATH_INFO']) {
+        case '/listar-cursos':
+            require 'listar-cursos.php';
+            break;
+        case '/novo-curso': 
+            require 'formulario-novo-curso.php';
+            break;
+        default:
+            echo "Página não encontrada, Erro 404";
+    }
